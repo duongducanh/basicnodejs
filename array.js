15f2220a912d21b6463fd679d5f352199dcbd17c
duc anh <anhdd@arrowhitech.com>
2017-03-15 23:55:15 +0700

    first commit

---
+++ array.js
@@ -0,0 +1,22 @@
+var arr=['bike', 'car', 'moto'];
+console.log(arr.valueOf());
+
+// for(var i=0; i<arr.length; i++){
+//     console.log(arr[i]); 
+// }
+//console.log(arr.join(" - "));
+
+arr.push('honda');
+console.log(arr);
+console.log(arr.pop());
+// while (arr.length > 0){
+//     arr.pop();
+//     console.log(arr.valueOf());
+// }
+
+// arr.splice(0, 1);
+// console.log(arr);
+
+// var cars = ["Saab", "Volvo", "BMW"];
+
+//     console.log(cars.reverse());
